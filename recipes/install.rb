29e9273fccdfa776376d2367ed4e07220ac7d9af
Luke Hertert <luke.hertert@gmail.com>
2015-05-26 16:40:40 -0400

    Use chocolatey cookbook

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,3 +1,5 @@
+include_recipe 'chocolatey'
+
 powershell_script 'Install IIS' do
   code 'Add-WindowsFeature Web-Server'
   not_if "(Get-WindowsFeature -Name Web-Server).Installed"
@@ -28,30 +30,32 @@ powershell_script 'Install Dynamic Compression' do
 	not_if "(Get-WindowsFeature -Name Web-Dyn-Compression).Installed"
 end	
 
-powershell_script 'Install Chocolatey' do
-	code  <<-EOH
-	iex ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1"))
-	$env:Path = "$($env:PATH);$($env:ALLUSERSPROFILE)\chocolatey\bin"
-	EOH
-	only_if '(get-command "choco.exe" -ErrorAction SilentlyContinue) -eq $null'
-end
+chocolatey 'UrlRewrite'
 
-powershell_script 'Install URL Rewrite' do
-	code 'choco install UrlRewrite -y'
-	#only_if '(choco list -localonly | Select-String "UrlRewrite") -eq $null'
-end
+#powershell_script 'Install Chocolatey' do
+#	code  <<-EOH
+#	iex ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1"))
+#	$env:Path = "$($env:PATH);$($env:ALLUSERSPROFILE)\chocolatey\bin"
+#	EOH
+#	only_if '(get-command "choco.exe" -ErrorAction SilentlyContinue) -eq $null'
+#end
 
-powershell_script 'Install Web Deploy' do
-	code 'choco install WebDeploy -y'
+#powershell_script 'Install URL Rewrite' do
+#	code 'choco install UrlRewrite -y'
+#	#only_if '(choco list -localonly | Select-String "UrlRewrite") -eq $null'
+#end
+
+#powershell_script 'Install Web Deploy' do
+#	code 'choco install WebDeploy -y'
 	#only_if '(choco list -localonly | Select-String "WebDeploy") -eq $null'
-end
+#end
 
-powershell_script 'Install Sublime Text 3' do
-	code 'choco install SublimeText3 -y'
+#powershell_script 'Install Sublime Text 3' do
+#	code 'choco install SublimeText3 -y'
 	#only_if '(choco list -localonly | Select-String "SublimeText3") -eq $null'
-end
+#end
 
-powershell_script 'Install 7zip' do
-	code 'choco install 7zip.install -y'
+#powershell_script 'Install 7zip' do
+#	code 'choco install 7zip.install -y'
 	#only_if '(choco list -localonly | Select-String "7zip.install") -eq $null'
-end
+#end
