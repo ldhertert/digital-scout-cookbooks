b55cf258c37363907c23fb3bd5f4fad1d2d1a867
Luke Hertert <luke.hertert@gmail.com>
2015-05-26 18:15:11 -0400

    Add new relic key

--- digital-scout/recipes/install.rb
+++ digital-scout/recipes/install.rb
@@ -22,31 +22,11 @@ windows_feature 'IIS-HttpCompressionDynamic' do
 end
 
 chocolatey 'UrlRewrite'
+chocolatey 'WebDeploy'
+chocolatey 'SublimeText3'
+chocolatey '7zip.install'
+chocolatey 'googlechrome'
 
-#powershell_script 'Install Chocolatey' do
-#	code  <<-EOH
-#	iex ((new-object net.webclient).DownloadString("https://chocolatey.org/install.ps1"))
-#	$env:Path = "$($env:PATH);$($env:ALLUSERSPROFILE)\chocolatey\bin"
-#	EOH
-#	only_if '(get-command "choco.exe" -ErrorAction SilentlyContinue) -eq $null'
-#end
-
-#powershell_script 'Install URL Rewrite' do
-#	code 'choco install UrlRewrite -y'
-#	#only_if '(choco list -localonly | Select-String "UrlRewrite") -eq $null'
-#end
-
-#powershell_script 'Install Web Deploy' do
-#	code 'choco install WebDeploy -y'
-	#only_if '(choco list -localonly | Select-String "WebDeploy") -eq $null'
-#end
-
-#powershell_script 'Install Sublime Text 3' do
-#	code 'choco install SublimeText3 -y'
-	#only_if '(choco list -localonly | Select-String "SublimeText3") -eq $null'
-#end
-
-#powershell_script 'Install 7zip' do
-#	code 'choco install 7zip.install -y'
-	#only_if '(choco list -localonly | Select-String "7zip.install") -eq $null'
-#end
+chocolatey 'newrelic-dotnet' do
+	args 'licensekey=df77750758d88ba07d962482cb3a6939c9309e23'
+end
