c256bb003f062a7393228ed81d709ab1b7e61a09
Luke Hertert <luke.hertert@gmail.com>
2015-05-26 20:11:35 -0400

    Artifacts

---
+++ digital-scout/recipes/deploy.rb
@@ -0,0 +1,7 @@
+remote_file "#{Chef::Config[:file_cache_path]}/hsgc/artifacts.zip" do
+  source "http://build.digitalscout.com:8080/job/GameCenterProduction/lastSuccessfulBuild/artifact/*zip*/archive.zip"
+end
+
+powershell_script "Unzip artifact" do 
+	code ' & "C:\Program Files\7-Zip\7z.exe" x "#{Chef::Config[:file_cache_path]}/hsgc/artifacts.zip" -o#{Chef::Config[:file_cache_path]}/hsgc/artifacts" -r -y'
+end
